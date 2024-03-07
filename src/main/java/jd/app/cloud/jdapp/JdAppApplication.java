0d3ae3161979a432324f163d8b09c4f7f6d9cc3b
DevOp-Edureka <kony.tahmina@gmail.com>
2024-03-07 16:40:09 -0500

    First Commit JD apps

---
+++ src/main/java/jd/app/cloud/jdapp/JdAppApplication.java
@@ -0,0 +1,13 @@
+package jd.app.cloud.jdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jd/app/cloud/jdapp/JdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jd.app.cloud.jdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
